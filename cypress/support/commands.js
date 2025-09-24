e79d3e67268c72cd1ee5a951e0e009ad53bbc268
M-Waleed-Ahmad-Axe <waleed.ahmad@axelliant.com>
2025-09-24 07:37:01 -0700

    Login Command Added to maintain Sessions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import facility_methods from '../Page-Objects/login.cy.js';
+
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.session([email, password], () => {
+        facility_methods.valid_login(email, password);
+    });
+    // Only visit after session is restored
+    cy.then(() => {
+        cy.visit('/');
+    });
+});
+
+// Cypress.Commands.add('login', (email, password) => {
+//   // Intercept login request to capture token
+//   cy.intercept('POST', '/api/v1/accounts/login/').as('loginRequest');
+
+//   cy.session([email, password], () => {
+//     const loginPage = new LoginPage();
+
+//     loginPage.visit();
+//     loginPage.login(email, password); // Should trigger the POST
+
+//     cy.wait('@loginRequest').then(({ response }) => {
+//       const token = response.body.data.access_token;
+//       cy.log('Access Token:', token);
+//       cy.task('storeAuthToken', token); // Store token in global variable
+//     });
+
+//     // Ensure login succeeded
+//     cy.url().should('include', '/dashboard');
+//   });
+
+//   // Always navigate to dashboard after restoring session
+//   cy.visit('/dashboard');
+// });
