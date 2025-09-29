73cf874dcba2f9ec483014a13c8a5c53cdb5bfce
M-Waleed-Ahmad-Axe <waleed.ahmad@axelliant.com>
2025-09-29 08:52:38 -0700

    Login Flow Separate and master flow file adjusted

--- cypress/e2e/Facility-Network/fn-full-flow.cy.js
+++ cypress/e2e/Facility-Network/fn-full-flow.cy.js
@@ -1,15 +1,2 @@
-import facility_methods from '../../Page-Objects/login.cy.js';
-
-describe('template spec', () => {
-  it('Log in with invalid email', () => {
-    facility_methods.invalid_email();
-  });
-
-  it('Log in with invalid password', () => {
-    facility_methods.invalid_password();
-  });
-
-  it('Should log in successfully with valid credentials', () => {
-    facility_methods.valid_login();
-  });
-});
+import '../login/login.cy.js';
+import '../work-orders/OrgAdmin.cy.js';
