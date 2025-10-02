682c0f4c3d0fd5bb54bb15968937993297c1bd8e
M-Waleed-Ahmad-Axe <waleed.ahmad@axelliant.com>
2025-10-02 07:50:38 -0700

    Users integrated in main

--- cypress/e2e/users/OrgAdmin.cy.js
+++ cypress/e2e/users/OrgAdmin.cy.js
@@ -9,21 +9,21 @@ describe('Org Admin - Users Page Tests', () => {
     });
 
     // Creation User Flows
-    // it('Should Add a new User', () => {
-    //     user_methods.inviteOneUser();
-    // });
+    it('Should Add a new User', () => {
+        user_methods.inviteOneUser();
+    });
 
-    // it('should add bulk users', () => {
-    //     user_methods.inviteBulkUsers(3);
-    // });
-    // it('should not allow inviting already invited user', () => {
-    //     user_methods.inviteAlreadyInvitedUser();
-    // });
+    it('should add bulk users', () => {
+        user_methods.inviteBulkUsers(3);
+    });
+    it('should not allow inviting already invited user', () => {
+        user_methods.inviteAlreadyInvitedUser();
+    });
 
-    // // Edit User Flows
-    // it('Should Edit an existing User', () => {
-    //     user_methods.editUser();
-    // });
+    // Edit User Flows
+    it('Should Edit an existing User', () => {
+        user_methods.editUser();
+    });
 
     it('Should Deactivate and activate an existing User', () => {
         user_methods.deactivateUser();
